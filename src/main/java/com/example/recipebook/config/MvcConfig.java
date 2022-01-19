bea91af17f3d0a67f69e4a1044c17c39b9480fa6
Yuliya Zhukova <eradiateyu@gmail.com>
2022-01-19 12:21:53 +0700

    Fix bug in autorization

--- src/main/java/com/example/recipebook/config/MvcConfig.java
+++ src/main/java/com/example/recipebook/config/MvcConfig.java
@@ -1,2 +1,12 @@
-package com.example.recipebook.config;public class MvcConfig {
+package com.example.recipebook.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 }
--- src/main/java/com/example/recipebook/domain/Role.java
+++ src/main/java/com/example/recipebook/domain/Role.java
@@ -1,2 +1,12 @@
-package com.example.recipebook.domain;public class Role {
+package com.example.recipebook.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
